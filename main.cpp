8035220801d276364cf27c7bbc0c8d0853dc7f87
DiegoSaragozaSilva <diegosaragozaa2@gmail.com>
2022-09-27 10:48:15 -0300

    GLM submodule and CMake build

--- main.cpp
+++ main.cpp
@@ -4,11 +4,13 @@
 #include <GLFW/glfw3.h>
 #include <vulkan/vulkan.hpp>
 #include <spdlog/spdlog.h>
+#include <glm/glm.hpp>
 
-#include "src/render_engine/Window.hpp"
+#include "src/render_engine/RenderEngine.hpp"
 
 int main() {
-    Window* window = new Window(640, 320, "New window"); 
-    delete window;
+    RenderEngine* renderEngine = new RenderEngine();
+    delete renderEngine;
+
     return 1;
 }
--- src/render_engine/RenderEngine.hpp
+++ src/render_engine/RenderEngine.hpp
@@ -1,6 +1,8 @@
 #ifndef _RENDER_ENGINE_H_
 #define _RENDER_ENGINE_H_
 
+#include "Window.hpp"
+
 class RenderEngine {
 public:
     RenderEngine();
